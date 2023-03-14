af05d0c26ad15d7d4c624e04cf240cadb0959d3e
Elisha Okon <eliscodavid@gmail.com>
2023-03-14 17:10:09 +0000

    here's a fix

---
+++ pages/staking-pool.js
@@ -0,0 +1,9 @@
+export default function StakingPool() {
+    return (
+        <section className="staking-pool">
+            <div className="mt-20 ml-20 text-center justify-center">
+                <BridgesApp />
+            </div>
+        </section>
+    );
+}
