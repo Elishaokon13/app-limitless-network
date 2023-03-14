72be27dbb05ab7135eeec5c2a9b2b38608102248
Elisha Okon <eliscodavid@gmail.com>
2023-03-14 17:26:04 +0000

    solved

--- pages/staking-pool.js
+++ pages/staking-pool.js
@@ -2,7 +2,7 @@ export default function StakingPool() {
     return (
         <section className="staking-pool">
             <div className="mt-20 ml-20 text-center justify-center">
-                <BridgesApp />
+                stake
             </div>
         </section>
     );
