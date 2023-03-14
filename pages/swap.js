34e35af522961b6eee76acb20ef86b54f6ed40ba
Elisha Okon <eliscodavid@gmail.com>
2023-03-14 17:02:56 +0000

    done with swap

--- pages/swap.js
+++ pages/swap.js
@@ -3,8 +3,7 @@ import { BridgesApp } from '@brgx/widget';
 export default function Swap() {
     return (
         <section className="swap">
-            
-            <h1 className="swap__title">Limitless Network Swap</h1>
+
             <div className="mt-20 ml-20 text-center justify-center">
                 <BridgesApp />
             </div>
