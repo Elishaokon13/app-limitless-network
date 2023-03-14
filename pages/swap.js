da008cf2526d5ecfbfef687387b5c6d01a969ab0
Elisha Okon <eliscodavid@gmail.com>
2023-03-14 16:54:36 +0000

    hiyo

---
+++ pages/swap.js
@@ -0,0 +1,13 @@
+import { BridgesApp } from '@brgx/widget';
+
+export default function Swap() {
+    return (
+        <section className="swap">
+            
+            <h1 className="swap__title">Limitless Network Swap</h1>
+            <div className="mt-20 ml-20 text-center justify-center">
+                <BridgesApp />
+            </div>
+        </section>
+    );
+}
