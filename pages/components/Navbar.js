39c2103433c1eb73d0b81e884253c30d75724791
Elisha Okon <eliscodavid@gmail.com>
2023-08-03 17:54:59 +0000

    done with connect wllet

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -113,9 +113,7 @@ export default function Example() {
                     </a>
                 </Popover.Group>
                 <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                        Log in <span aria-hidden="true">&rarr;</span>
-                    </a>
+                    <ConnectWallet />
                 </div>
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -187,12 +185,7 @@ export default function Example() {
                                 </a>
                             </div>
                             <div className="py-6">
-                                <a
-                                    href="#"
-                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                >
-                                    Log in
-                                </a>
+                                <ConnectWallet />
                             </div>
                         </div>
                     </div>
