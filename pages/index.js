5eaa6c471892e858c592c04905f5efccb70f586c
Elisha Okon <eliscodavid@gmail.com>
2023-08-04 21:10:23 +0000

    done some changes

--- pages/index.js
+++ pages/index.js
@@ -62,25 +62,26 @@ export default function Home() {
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   };
 
+  const formatNumber = (num) => {
+    const tier = Math.log10(num) / 3 | 0;
+    if (tier === 0) return num.toFixed(0);
+    const scale = Math.pow(10, tier * 3);
+    const scaled = num / scale;
+    return scaled.toFixed(2);
+  };
+
   useEffect(() => {
-    const fetchTotalSupply = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch('https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=0x593649f70f836565e33f0bce9af9503c243359b3&apikey=RK9BKPUGPM4MIXZBXK4QWTYCW3YRN3WGKF');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
         const data = await response.json();
-        setTotalSupply(data.result);
+        const totalSupplyValue = data.result && !isNaN(data.result) ? Number(data.result) : 0;
+        setTotalSupply(formatNumber(totalSupplyValue));
       } catch (error) {
-        console.error('Error fetching total supply:', error);
+        console.error('Error fetching coin data:', error);
       }
     };
-
-    // Example data for liquidity
-    const liquidityData = "5000";
-
-    setLiquidity(liquidityData);
-    fetchTotalSupply();
+    fetchData();
   }, []);
 
   return (
@@ -98,7 +99,7 @@ export default function Home() {
               </div>
               <p className='text-sm text-[#14c2a3]'>BSC</p>
             </div>
-            <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
+            {/* <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
               <div className=' flex items-center justify-between w-full'>
                 <p className='text-xl'>Bridge liquidity</p>
                 {liquidity !== null ? (
@@ -108,7 +109,7 @@ export default function Home() {
                 )}
               </div>
               <p className='text-sm text-[#14c2a3]'>Locked supply</p>
-            </div>
+            </div> */}
           </div>
         </div>
         {/* our token */}
