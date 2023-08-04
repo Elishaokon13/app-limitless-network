a1487790abc1fddd2d444c9b4054b2a8e0d89dd3
Elisha Okon <eliscodavid@gmail.com>
2023-08-04 19:59:44 +0000

    claim

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
 import { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Image from "next/image";
+import Claim from './components/Claim';
+// Placeholder components, you should replace these with actual implementations
+// const Claim = () => <p>Claim component</p>;
+// const BalanceOf = () => <p>BalanceOf component</p>;
+// const DividendHolders = () => <p>DividendHolders component</p>;
+// const WithdrawableDividend = () => <p>WithdrawableDividend component</p>;
 
 export default function Home() {
   const [totalSupply, setTotalSupply] = useState(null);
   const [liquidity, setLiquidity] = useState(null);
+  const [showClaimButton, setShowClaimButton] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(0);
+
+  useEffect(() => {
+    let countdownInterval;
+
+    const startCountdown = () => {
+      const countdownTime = 3 * 7 * 24 * 60 * 60; // 3 weeks in seconds
+      setTimeRemaining(countdownTime);
+
+      countdownInterval = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    };
+
+    const handleRestartCountdown = () => {
+      clearInterval(countdownInterval);
+      setShowClaimButton(false);
+      startCountdown();
+    };
+
+    startCountdown();
+
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setShowClaimButton(true);
+      setTimeout(() => {
+        setShowClaimButton(false);
+        setTimeRemaining(3 * 7 * 24 * 60 * 60); // Reset countdown to 3 weeks
+      }, 60 * 60 * 1000); // 1 hour in milliseconds
+    }
+  }, [timeRemaining]);
+
+  // Format time to display in the format "HH:mm:ss"
+  const formatTime = (time) => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = Math.floor(time % 60);
+
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  };
 
   useEffect(() => {
     const fetchTotalSupply = async () => {
@@ -28,36 +84,67 @@ export default function Home() {
   }, []);
 
   return (
-   <>
-    <Navbar />
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-
-        <div className='grid lg:grid-cols-2 gap-3 w-full text-white'>
-          <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
-            <div className=' flex items-center justify-between w-full'>
-              <p className='text-xl'>Total supply</p>
-              <p className='text-2xl'>{totalSupply !== null ? totalSupply : 'Loading...'}B</p>
+    <>
+      <Navbar />
+      <div className='bg-black w-{150px} lg:pr-16 flex flex-col gap-8'>
+        {/* our supply */}
+        <div className='flex flex-col gap-3 items-start  w-full '>
+          <p >Our supply</p>
+          <div className='grid lg:grid-cols-2 gap-3 w-full text-white'>
+            <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
+              <div className=' flex items-center justify-between w-full'>
+                <p className='text-xl'>Total supply</p>
+                <p className='text-2xl'>{totalSupply}B</p>
+              </div>
+              <p className='text-sm text-[#14c2a3]'>BSC</p>
             </div>
-            <p className='text-sm text-[#14c2a3]'>BSC</p>
-          </div>
-
-          <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
-            <div className=' flex items-center justify-between w-full'>
-              <p className='text-xl'>Bridge liquidity</p>
-              {liquidity !== null ? (
-                <p className='text-2xl'>${liquidity}</p>
-              ) : (
-                <p className='text-2xl'>Loading...</p>
-              )}
+            <div className='rounded-lg bg-[#152a3b] px-6 p-3 border-dashed flex flex-col gap-3 justify-center items-start h-[150px]'>
+              <div className=' flex items-center justify-between w-full'>
+                <p className='text-xl'>Bridge liquidity</p>
+                {liquidity !== null ? (
+                  <p className='text-2xl'>${liquidity}</p>
+                ) : (
+                  <p className='text-2xl'>Loading...</p>
+                )}
+              </div>
+              <p className='text-sm text-[#14c2a3]'>Locked supply</p>
             </div>
-            <p className='text-sm text-[#14c2a3]'>Locked supply</p>
           </div>
         </div>
+        {/* our token */}
+        <div className='flex flex-col gap-2 items-start  w-full '>
+          <p >Our supply</p>
+          <div className='grid lg:grid-cols-3 gap-3 w-full text-white'>
+            <div className='rounded-lg bg-[#152a3b]  border-dashed flex flex-col gap-3 items-center justify-center h-[200px]'>
+              <p className='text-xl'>Claim Rewards(48hours)</p>
 
+              {showClaimButton && (
+                <Claim />
+              )}
+            </div>
+            {/* <div className='rounded-lg bg-[#152a3b]  border-dashed flex flex-col gap-3 items-center justify-center h-[200px]'>
+              <p className='text-xl'>Your Dividend holdings </p>
+
+              <BalanceOf />
+
+            </div> */}
+            <div className='rounded-lg bg-[#152a3b]  border-dashed flex flex-col gap-3 items-center justify-center h-[200px]'>
+              <p className='text-xl'>Next Payout</p>
+              <div>{formatTime(timeRemaining)}</div>
+            </div>
+            {/* <div className='rounded-lg bg-[#152a3b]  border-dashed flex flex-col gap-3 items-center justify-center h-[200px]'>
+              <p className='text-xl'>Dividend holders</p>
+              <DividendHolders />
+            </div>
+            <div className='rounded-lg bg-[#152a3b]  border-dashed flex flex-col gap-3 items-center justify-center h-[200px]'>
+              <p className='text-xl'>Total Rewards</p>
+              <WithdrawableDividend />
+            </div> */}
+
+
+          </div>
+        </div>
       </div>
-    </main>
     </>
   );
 }
