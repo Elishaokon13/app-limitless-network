9c6b4f2dc2a7d085e8bb48ac77dc587eac5b7785
Elisha Okon <eliscodavid@gmail.com>
2023-08-06 21:23:42 +0000

    tried creating a manual claim button

--- pages/components/Claim.js
+++ pages/components/Claim.js
@@ -1,14 +1,44 @@
-import { Web3Button } from "@thirdweb-dev/react";
+import { useEffect } from 'react';
+import Web3 from 'web3';
+import contractABI from '../contractABI.json'; // Replace with the path to your contract ABI JSON file
 
-export default function Claim() {
+const Claim = () => {
+  useEffect(() => {
+    // Load Web3 using the injected provider from the user's wallet (e.g., Metamask)
+    if (typeof window !== 'undefined' && window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    }
+  }, []);
+
+  const executeClaimFunction = async () => {
+    if (!window.web3) {
+      alert('Please connect your wallet (e.g., Metamask) to use this feature.');
+      return;
+    }
+
+    const contractAddress = process.env.CONTRACT_ADDRESS;
+    const contractABI = JSON.parse(process.env.CONTRACT_ABI);
+    const contract = new window.web3.eth.Contract(contractABI, contractAddress);
+
+    try {
+      const accounts = await window.web3.eth.getAccounts();
+      const account = accounts[0];
+
+      // Call the claim function of the contract
+      const result = await contract.methods.claim().send({ from: account });
+
+      // Handle the result as needed
+      console.log('Transaction successful:', result);
+    } catch (error) {
+      console.error('Error executing claim function:', error);
+    }
+  };
+
   return (
-    <Web3Button
-      contractAddress="0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
-      action={(contract) => {
-        contract.call("claim")
-      }}
-    >
-      Claim
-    </Web3Button>
-  )
-}
+    <div>
+      <button onClick={executeClaimFunction}>Claim Tokens</button>
+    </div>
+  );
+};
+
+export default Claim;
