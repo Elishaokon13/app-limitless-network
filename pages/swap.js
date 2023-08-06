945895c420add74584c3391cd7b04ffc1e8eeae2
Elisha Okon <eliscodavid@gmail.com>
2023-08-06 20:09:40 +0000

    made some changes

--- pages/swap.js
+++ pages/swap.js
@@ -3,39 +3,38 @@ import Navbar from './components/Navbar';
 
 export default function Swap() {
     return (
-        <section className="swap">
+        <>
             <Navbar />
-            <h1 className="swap__title">Limitless Network Swap</h1>
-            <div className="mt-20 ml-20 text-center justify-center">
+            <section className="bg-black lg:w-3/4 xl:w-2/3 mx-auto px-4 py-8 flex flex-col gap-8">
+                
+                <div className="text-center justify-center">
+                    <PoodlApp
 
-
-
-                <PoodlApp
-                   
-                    predefinedTokens={[
-                        {
-                            address: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
-                            chainId: 56,
-                            decimals: 18,
-                            hasTransactonFees: true,
-                            logoURI: "https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840",
-                            name: "Limitless Network",
-                            symbol: "LNT"
-                        }
-                    ]}
-                    defaultPinnedTokens={{
-                        56: [
-                            "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
-                            "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-                        ]
-                    }}
-                    defaultSelectedTokenByChains={{
-                        56: {
-                            defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
-                        }
-                    }}
-                />
-            </div>
-        </section>
+                        predefinedTokens={[
+                            {
+                                address: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
+                                chainId: 56,
+                                decimals: 18,
+                                hasTransactonFees: true,
+                                logoURI: "https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840",
+                                name: "Limitless Network",
+                                symbol: "LNT"
+                            }
+                        ]}
+                        defaultPinnedTokens={{
+                            56: [
+                                "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
+                                "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+                            ]
+                        }}
+                        defaultSelectedTokenByChains={{
+                            56: {
+                                defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+                            }
+                        }}
+                    />
+                </div>
+            </section>
+        </>
     );
 }
