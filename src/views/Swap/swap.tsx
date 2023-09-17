d26660552b8afacbadbe4130ab3ae261ca9e9fc9
thebridgesengineer <111656632+thebridgesengineer@users.noreply.github.com>
2023-09-17 17:23:20 +0100

    Error fix

--- src/views/Swap/swap.tsx
+++ src/views/Swap/swap.tsx
@@ -3,107 +3,107 @@ import PageTitle from 'components/PageTitle'
 import {Flex, FlexItem } from 'uikit'
 import { PoodlApp } from '@poodl/widget';
 import useWindowDimensions from 'hooks/useWindowDimensions';
-
-const SwapTitle = () => {
-  return(
-    <Flex width="100%" maxWidth={{ xs: '145px', md: '175px' }}>
-    <div style={{display: "flex"}}>
-      <div style={{flex: 1}}>
-        <img src="https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840" alt="logo" style={{height: "45px"}} />
-      </div>
-      <div style={{flex: 2, alignSelf: "center"}}>
-        <div>
-          <p style={{fontSize: "18px"}}>Limitless Swap</p>
-        </div>
-      </div>
-    </div>
-  </Flex>
-  )
-}
-
-const handleTokenSelect = (outputToken, inputToken) => {    
-  if (outputToken && outputToken.symbol === "LNT" || inputToken && inputToken.symbol === "LNT"){
-    return true
-  } else {
-    return false
-  }
-}
-
-const SwapWidget = () => {
-  return(
-    <>
-      <PoodlApp
-        onTokenSelect={handleTokenSelect}
-        primaryColorDark='#2196f3'
-        borderRadius={5}
-        enableDarkMode={true}   
-        predefinedTokens={[
-            {
-                address: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
-                chainId: 56,
-                decimals: 18,
-                hasTransactonFees: true,
-                logoURI: "https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840",
-                name: "Limitless Network",
-                symbol: "LNT"
-            }
-        ]}
-        defaultPinnedTokens={{
-            56: [
-                "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
-                "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-            ]
-        }}
-        defaultSelectedTokenByChains={{
-            56: {
-                defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
-            }
-        }}
-        overrides={{
-          SwapCard: {
-            sx: {
-              backgroundColor: '#000000',
-              borderColor: 'secondary.main',
-              minWidth: '100%'
-            }
-          },
-          SwapCardTitle: <SwapTitle/>
-        }}  
-      />
-    </>
-  )
-}
-
-
-const Chart = () => {
-  const url = "https://coinbrain.com/embed/bnb-0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead?theme=custom&accent=2196f3&padding=16&background=000000&chart=1&trades=1"
-  return(
-    <>
-    <iframe title="lntChart" width="100%" height="850" src={url}></iframe>
-    </>
-  )
-}
-
-
-const Stats = () => {
-  const { width } = useWindowDimensions();
-  const statsHeight = (): string => {
-    if (width < 852){
-      return '500px'
-    } else {
-      return "100%"
-    }
-  }
-  const widgetHeight = statsHeight()
-  const url = "https://coinbrain.com/coins/bnb-0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead/ticker?theme=custom&accent=2196f3&background=000000&padding=16&type=large&currency=USD&blocks=price%2CmarketCap%2Cvolume24h%2Cliquidity"
-  return(
-    <>
-    <iframe title="lntStats" width="100%" height={widgetHeight}  src={url}></iframe>
-    </>
-  )
-}
+import { useState } from 'react';
 
 const Swap = () => {
+
+  const [lntActive, setLntActive] = useState(true);
+
+  const SwapTitle = () => {
+    return(
+      <Flex width="100%" maxWidth={{ xs: '145px', md: '175px' }}>
+      <div style={{display: "flex"}}>
+        <div style={{flex: 1}}>
+          <img src="https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840" alt="logo" style={{height: "45px"}} />
+        </div>
+        <div style={{flex: 2, alignSelf: "center"}}>
+          <div>
+            <p style={{fontSize: "18px"}}>Limitless Swap</p>
+          </div>
+        </div>
+      </div>
+    </Flex>
+    )
+  }
+
+  const handleTokenSelect = (outputToken, inputToken) => {    
+    (outputToken && outputToken.symbol === "LNT" || inputToken && inputToken.symbol === "LNT") ? setLntActive(true) : setLntActive(false)
+  }
+
+  const SwapWidget = () => {
+    return(
+      <>
+        <PoodlApp
+          onTokenSelect={handleTokenSelect}
+          primaryColorDark='#2196f3'
+          borderRadius={5}
+          enableDarkMode={true}   
+          predefinedTokens={[
+              {
+                  address: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
+                  chainId: 56,
+                  decimals: 18,
+                  hasTransactonFees: true,
+                  logoURI: "https://www.limitlessnetwork.org/_next/static/media/Logo.3a89c1e7.png?imwidth=3840",
+                  name: "Limitless Network",
+                  symbol: "LNT"
+              }
+          ]}
+          defaultPinnedTokens={{
+              56: [
+                  "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD",
+                  "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+              ]
+          }}
+          defaultSelectedTokenByChains={{
+              56: {
+                  defaultOutputToken: "0xC13CbF50370E5EaE6f5Dd9D8a1015007f34C4eaD"
+              }
+          }}
+          overrides={{
+            SwapCard: {
+              sx: {
+                backgroundColor: '#000000',
+                borderColor: 'secondary.main',
+                minWidth: '100%'
+              }
+            },
+            SwapCardTitle: <SwapTitle/>
+          }}  
+        />
+      </>
+    )
+  }
+
+
+  const Chart = () => {
+    const url = "https://coinbrain.com/embed/bnb-0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead?theme=custom&accent=2196f3&padding=16&background=000000&chart=1&trades=1"
+    return(
+      <>
+      <iframe title="lntChart" width="100%" height="850" src={url}></iframe>
+      </>
+    )
+  }
+
+
+  const Stats = () => {
+    const { width } = useWindowDimensions();
+    const statsHeight = (): string => {
+      if (width < 852){
+        return '500px'
+      } else {
+        return "100%"
+      }
+    }
+    const widgetHeight = statsHeight()
+    const url = "https://coinbrain.com/coins/bnb-0xc13cbf50370e5eae6f5dd9d8a1015007f34c4ead/ticker?theme=custom&accent=2196f3&background=000000&padding=16&type=large&currency=USD&blocks=price%2CmarketCap%2Cvolume24h%2Cliquidity"
+    return(
+      <>
+      <iframe title="lntStats" width="100%" height={widgetHeight}  src={url}></iframe>
+      </>
+    )
+  }
+
   return (
     <Page m={{ xs: '44px 0px', sm: '0px' }}>
       <PageTitle
@@ -120,13 +120,17 @@ const Swap = () => {
           <SwapWidget />
         </FlexItem>
 
-        <FlexItem xs={12} md={6}>
-          <Stats/>
-        </FlexItem>    
+        {lntActive ? (
+          <>
+          <FlexItem xs={12} md={6}>
+            <Stats/>
+          </FlexItem>    
 
-        <FlexItem xs={12} md={12}>
-          <Chart/>
-        </FlexItem>    
+          <FlexItem xs={12} md={12}>
+            <Chart/>
+          </FlexItem>  
+          </> 
+        ) : (undefined) }
 
       </Flex>
       <br />
